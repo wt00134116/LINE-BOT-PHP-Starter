c95602a5e9b73b73376f0206b69e0702ac1f7e4e
wt00134116 <93109587+wt00134116@users.noreply.github.com>
2022-02-16 12:07:45 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'gFj42cIxJjJO14XZXRXliXQSSA2fSnzjncY6dudRY8o/LVoHMXEVh0c1OpBEeI9rrHhZSUFX5g8N4BEz+++4zzaLYNAYpcA5jZI7+wB43Q0LxkynrkUlkRXvFO+Nyxu0z49PtyxKTFvrWLdnHtiETwdB04t89/1O/w1cDnyilFU=';
+$access_token = 'cy8uWvKZ+vrFXXgLPWLDGYJHVb7M2V7t3GihlsWJlDrh7W+y9rK3P/x0Dt4iQcYOEwSi6MT60XgoS7+GffKaZiGiW9Up9ufD2oOL/3XVjBcZqxhI8voNmpzDpmH0KeE0Ern+ByTJz/c5DfQQqDlaZgdB04t89/1O/w1cDnyilFU=';
  
 $content = file_get_contents('php://input');
 $arrJson = json_decode($content, true);
